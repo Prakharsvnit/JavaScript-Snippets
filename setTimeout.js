5f18983756e8512f019fa78fa7099692dae54112
Prakhar tripathi <19989448+Prakharsvnit@users.noreply.github.com>
2022-12-30 17:43:06 +0000

    Added setTimeout code

---
+++ setTimeout.js
@@ -0,0 +1,28 @@
+/*
+setTimeout is asynchronous method
+which executes a function after specified time,
+only ONCE.
+*/
+
+const greet = () => {
+  console.log("I will appear second");
+}
+
+console.log("this will appear first")
+setTimeout(greet,3000);
+
+/* setTimeout function returns timerId
+which is given as arguement to clearTimeout.
+*/
+
+let timerId = setTimeout(greet,6000);
+console.log(timerId)
+
+clearTimeout(timerId)
+console.log("timer is stopped")
+
+/*
+You generally use the clearTimeout() 
+method when you need to cancel the 
+setTimeout() method call before it happens.
+*/
