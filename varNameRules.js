7cbab435e63e2173bc413284017c1be123c20f29
Prakhartripath9 <>
2023-01-16 06:07:47 +0000

    variable naming rules

---
+++ varNameRules.js
@@ -0,0 +1,13 @@
+var spider man = "Peter Parker"
+console.log(spider man)
+// don't use spaces
+
+var new = "Mine year"
+console.log(new)
+
+var class = "intermediate"
+console.log(class)
+//don't use keywords
+
+//Use alphanumeric,special characters and
+//names starting with $ or _ sign
