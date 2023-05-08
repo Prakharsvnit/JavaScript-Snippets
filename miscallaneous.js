cae62a4372eabe98d081109ae5643159e4ae2d3c
Prakharsvnit <tprakhar468@gmail.com>
2023-05-08 11:50:52 +0000

    Add typeConversion.js

--- miscallaneous.js
+++ miscallaneous.js
@@ -1,2 +1,16 @@
-let obj = null;
-console.log(typeof obj)
+let a = null;
+console.log(typeof a) //Object!!! 
+
+console.log(null == undefined) //true
+console.log(null === undefined) //false
+
+let counter
+console.log(counter) // undefined
+console.log(typeof counter) // undefined
+
+console.log(typeof undeclared_variable)
+
+let object1 = {name:"peter"}
+console.log(typeof object1)
+
+console.log(typeof NaN) //number
---
+++ typeConversion.js
@@ -0,0 +1,41 @@
+/*
+String Conversion
+Occurs when we output something. Can be performed with String(value)
+*/
+
+let value = true
+console.log(value,typeof value)
+
+value = String(value)
+console.log(value,typeof value)
+
+/*
+Numeric conversion
+Occurs in math operations. Can be performed with Number(value)
+*/
+
+let age = Number("an arbitrary string instead of a number");
+
+console.log(age); // NaN, conversion failed
+
+console.log( Number(true) );        // 1
+console.log( Number(false) );       // 0
+
+console.log(Number(null))           // 0
+console.log(Number(undefined))     // NaN
+
+/*
+Boolean Conversion
+Occurs in logical operations. Can be performed with Boolean(value)
+*/
+
+console.log(Boolean(0))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+console.log(Boolean(NaN))
+console.log(Boolean(""))
+console.log(Boolean(false))
+
+//output for above are false
+
+console.log(Boolean(true))  //true
