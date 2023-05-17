84b0d965c4cd5bfa8626795ce9b794473141a3a2
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-17 00:52:21 +0000

    Updated miscallaneous.js

--- miscallaneous.js
+++ miscallaneous.js
@@ -8,7 +8,7 @@ let counter
 console.log(counter) // undefined
 console.log(typeof counter) // undefined
 
-console.log(undeclared_variable) //ReferenceError
+// console.log(undeclared_variable) //ReferenceError
 console.log(typeof undeclared_variable) //undefined
 
 //You will have to comment line 11 to make rest pf code work (Wierd!)
@@ -25,3 +25,18 @@ console.log(c) //0.30000000000000004
 
 let b = 0.1 + 0.21
 console.log(b) //0.31
+
+console.log(10+20) //30
+console.log('10' + '20') //1020
+console.log(10 + '20')   //1020
+
+//Addition of anything with NaN results in NaN
+console.log(NaN + 10)  //NaN
+console.log(NaN + NaN) //NaN
+
+console.log('5' * 2) //10 (Wierd!)
+console.log(Infinity * 0) //NaN
+console.log(Infinity * 2) //Infinity
+
+console.log(Infinity / Infinity)  //NaN
+console.log(0/0)                  //NaN
