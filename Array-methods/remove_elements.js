ebe6bd54da12d2bffc3a8cb2b95ac3a05cb4729a
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-23 16:01:39 +0000

    Added slice method in arrays

--- Array-methods/remove_elements.js
+++ Array-methods/remove_elements.js
@@ -1,4 +1,4 @@
-// remove elements in array : pop(),shift()
+// remove elements in array : pop(),shift(),slice()
 
 // pop() method removes the last element from an array and returns the removed element
 const numbers = [10, 20, 30];
@@ -13,3 +13,8 @@ let number = numbers_list.shift();
 
 console.log( number);                  // 10
 console.log( numbers_list );           // [20,30]
+
+// slice() method removes a part of array within given indexes
+var colors = ['red','green','blue','yellow','purple'];
+var rgb = colors.slice(0,3);
+console.log(rgb); // ["red", "green", "blue"]
