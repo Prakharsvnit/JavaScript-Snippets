3070001a5ab477abe90627355821d795789ae1b4
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-24 06:21:49 +0000

    Added map() method

---
+++ HighOrder-methods/map.js
@@ -0,0 +1,13 @@
+// map() method iterates over elements and transforms them according to callback function
+// and RETURNS NEW Array
+
+// Example 1
+let numbers = [16, 25, 36];
+let result = numbers.map(number => Math.sqrt(number))
+console.log(result);     // [4,5,6]
+
+
+// Exmaple 2
+let circles_radius = [10, 30, 50];
+let areas = circles_radius.map(radius => Math.floor(Math.PI * radius * radius));
+console.log(areas);        // [ 314, 2827, 7853 ]
