7dadbef65e20d62e6375bde19244bf14d4f295ed
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-24 07:12:23 +0000

    Added forEach() method

---
+++ HighOrder-methods/forEach.js
@@ -0,0 +1,6 @@
+// forEach() method to exeucte a function on every element in an array.
+// It is similar to map() method but it DOESN'T ALTER ORIGINAL ARRAY
+
+let prices = [1800, 2000, 3000, 5000, 500, 8000];
+prices.forEach((value,index) => console.log(index + ":" + value)); 
+prices.forEach(ele => ele + 10);   // No Output because forEach() method doesn't alter original array,use map() method for this
--- HighOrder-methods/map.js
+++ HighOrder-methods/map.js
@@ -1,13 +1,14 @@
 // map() method iterates over elements and transforms them according to callback function
-// and RETURNS NEW Array
+// and RETURNS NEW Array,DOESN'T ALTERS ORIGINAL ARRAY 
 
 // Example 1
 let numbers = [16, 25, 36];
 let result = numbers.map(number => Math.sqrt(number))
 console.log(result);     // [4,5,6]
-
+console.log(numbers)     // [16,25,36]
 
 // Exmaple 2
 let circles_radius = [10, 30, 50];
 let areas = circles_radius.map(radius => Math.floor(Math.PI * radius * radius));
-console.log(areas);        // [ 314, 2827, 7853 ]
+console.log(areas);           // [ 314, 2827, 7853 ]
+console.log(circles_radius)   // [ 10, 30, 50 ]
