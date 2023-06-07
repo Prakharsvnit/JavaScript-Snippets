ae8fd46e97209b5fead03ee9e3164bd515e0b2e5
Prakhar tripathi <tprakhar468@gmail.com>
2023-06-07 11:18:39 +0000

    Updated Objects examples

--- Objects/basics.js
+++ Objects/basics.js
@@ -1,18 +1,26 @@
+// Object is an unordered list of key-value pairs. 
+// The key is usually a string or a symbol. 
+// The value can be of type -> string, boolean, number, undefined,null,object,function.
+
+
 // Object example
 const student = { 
-    name: 'John',                         // name -> key,'John' -> value ; "key: value" pairs are called properties
-    age: 20,
-    marks: {
-        science: 70,
-        math: 75
-    }
-}   
+  name: 'John',                         
+  age: 20,
+  marks: {science: 70, math: 75},
+  percentage: function() {
+      const totalMarks = Object.values(this.marks).reduce((acc, val) => acc + val, 0);
+      const percentage = (totalMarks / (Object.keys(this.marks).length * 100)) * 100;
+      return percentage;
+  }
+}
 
 // Access object properties
 console.log(student.name)                    // John
 console.log(student["name"])                 // John
 console.log(student.marks.science)           // 70
 console.log(student["marks"]["science"])     // 70
+console.log(student.percentage());           // 72.5
 
 // Ways to create Objects
 let obj = Object.create(null);            // Object's create method
@@ -24,11 +32,11 @@ function Person(name,age) {               // Function constructor
 }
 let person_obj = new Person("Peter",26);
 
-class Person {                            // ES6 class syntax
+class Individual {                            // ES6 class syntax
     constructor(name) {
       this.name = name;
     }
   }
   
-let person_object = new Person("Sudheer");
+let person_object = new Individual("Peter");
 
