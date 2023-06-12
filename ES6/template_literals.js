239f216575ab9eb52e983d5128bc61007a879a4b
Prakhar tripathi <tprakhar468@gmail.com>
2023-06-12 03:58:13 +0000

    Added Template literla method in strings

---
+++ ES6/template_literals.js
@@ -0,0 +1,16 @@
+// create a template literal by wrapping your text in backticks (`)
+
+/*
+Template literals allow these amazing features:
+1. String that can span multiple lines.
+2. Substitute part of the string for the values of variables or expressions.
+*/
+
+let simple = `The world is constantly evolving, and technology is at 
+the forefront of this change. From AI to automation, the way we live and work 
+is being transformed by the latest innovations.`;
+
+
+let firstName = 'John',lastName = 'Doe';
+let greeting = `Hi ${firstName}, ${lastName}`;
+console.log(greeting); // Hi John, Doe
