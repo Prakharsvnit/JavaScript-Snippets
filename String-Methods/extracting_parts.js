5df0a942c9128b6525fe92335d37428a4f00fd66
Prakhar tripathi <tprakhar468@gmail.com>
2023-06-25 13:33:44 +0530

    [WIP] extracting_part methods of string

--- String-Methods/extracting_parts.js
+++ String-Methods/extracting_parts.js
@@ -1,28 +1,21 @@
 /*
-.split() method takes in separator(comma,underscore,full-stop,hyphen,whitespace) 
-as parameter and outputs the input string as array of strings with separator removed. 
-*/
-
-const fruits = "apple,banana,orange";
-const fruitArray = fruits.split("-");  // can be separated using split
-console.log(fruitArray); // Output: ["apple", "banana", "orange"]
-
-/*
 .substring() and .slice() method take startIndex and endIndex as parameters
 and output the substring between those indexes.
 NOTE : output string has this range of index -> [startIndex,endIndex)
 */
 
+// TODO : Add .split() and .join() method
+
 const str = "Hello, world";
-console.log(str.slice(7,12));
-console.log(str.substring(7,12));
-console.log(str.slice(7))
-console.log(str.substring(7))
+console.log(str.slice(7, 12));
+console.log(str.substring(7, 12));
+console.log(str.slice(7));
+console.log(str.substring(7));
 
-// Same output for above -> world 
+// Same output for above -> world
 
-console.log(str.slice())
-console.log(str.substring())
+console.log(str.slice());
+console.log(str.substring());
 
 // Same output for above -> Hello, world
 
@@ -33,9 +26,9 @@ Differences between .slice() and .substring() :
 - If the endIndex is negative, the slice() method treats it as str.length + end
 */
 
-console.log(str.length)              //12
+console.log(str.length); //12
 
-console.log(str.substring(12,7));   //world
-console.log(str.slice(12,7))        //"" (Empty string)
-console.log(str.slice(-5));         //world                    
-console.log(str.slice(0,-7))        //Hello
+console.log(str.substring(12, 7)); //world
+console.log(str.slice(12, 7)); //"" (Empty string)
+console.log(str.slice(-5)); //world
+console.log(str.slice(0, -7)); //Hello
