4657f0fc2b95a1189fc4e32f9610ee8715220655
Prakhar tripathi <tprakhar468@gmail.com>
2023-06-27 16:35:22 +0000

    Updated Nullish coalescing operator

--- ES6/nullish_coalescing_operator.js
+++ ES6/nullish_coalescing_operator.js
@@ -1,10 +1,12 @@
 // Coalescing refers to the process of merging or combining two or more things into a single entity.
+//  It treats null and undefined as “nullish” and all other values, 
+// including false, NaN, 0, or an empty string (''), as non-nullish
 
 /*
 const result = value1 ?? value2;
 
 Nullish coalescing operator returns value2 
-if value1 is null or undefined else value1
+if value1 is nullish else value1
 The logic transfers to:
 
 const result = value1;
@@ -18,7 +20,7 @@ console.log(undefined ?? 56);        //  56
 console.log(null ?? 0);              //  0
 console.log(45 ?? 25);               //  45
 
-let result1 = 1 ?? console.log('Hi');
+let result1 = 0 ?? console.log('Hi');
 console.log(result1);               // 1
 
 // REMARK: I think it's the best way to check for null and undefined values
