38ce418d16deb7096fcdc5ca2b659f984c667142
Prakhar tripathi <tprakhar468@gmail.com>
2024-01-16 13:45:48 +0000

    added some and every array methods

---
+++ ArrayMethods/someEvery.js
@@ -0,0 +1,18 @@
+// some() method checks if at least one element in the array passes a test.
+
+const marks = [ 4, 5, 7, 9, 10, 3 ];
+
+const lessThanFive = marks.some((e) => {
+    return e < 5;
+});
+
+console.log("output from some()",lessThanFive);
+
+// every() method checks If All Array Elements Pass a Test
+// considering above example
+
+const equalToEight = marks.every((e) => {
+    return e == 8;
+})
+
+console.log("output from every()",equalToEight)
