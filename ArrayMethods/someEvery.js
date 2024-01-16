8c2eeea95ba0d1e3e4da0a988f608d702519e45d
Prakhar tripathi <tprakhar468@gmail.com>
2024-01-16 13:57:15 +0000

    explanation of output on empty arrays for some,every methods

--- ArrayMethods/someEvery.js
+++ ArrayMethods/someEvery.js
@@ -17,5 +17,13 @@ const equalToEight = marks.every((e) => {
 
 console.log("output from every()",equalToEight)
 
-// Caution: If the input array is empty, conditions don't matter
-// In case of every(),result will always be true
+/* Caution: 
+If the input array is empty, conditions don't matter
+In case of every(),result will always be true, and in case of some() method,result will always be false.
+
+Since there are no elements in the array, the callback function is never executed, and the .every() method considers 
+the condition to be satisfied for all elements (as there are none), since there are no elements that violate the condition,so the output is always true
+
+The some() method checks for the existence of at least one element that satisfies the given condition, and an empty array contains no elements.
+the condition is never satisfied. The output will be false.
+*/
